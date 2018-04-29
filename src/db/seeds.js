27ad07930d87bc1ac4f80d72e614048bb44301dd
Christiebelle <cslester@outlook.com>
2018-04-29 17:44:27 +0100

    titles for each topic added

--- src/db/seeds.js
+++ src/db/seeds.js
@@ -3,7 +3,52 @@ use black_death_db;
 db.dropDatabase();
 
 db.facts.insert({
-	title: "",
+	title: "What was the Black Death?",
+	stat: "",
+	information: "",
+},
+{
+	title: "What were the symptoms?",
+	stat: "",
+	information: "",
+},
+{
+	title: "How did it spread?",
+	stat: "",
+	information: "",
+},
+{
+	title: "Why did it spread?",
+	stat: "",
+	information: "",
+},
+{
+	title: "What was the death toll?",
+	stat: "",
+	information: "",
+},
+{
+	title: "Who/What were Plague doctors?",
+	stat: "",
+	information: "",
+},
+{
+	title: "Was there a cure?",
+	stat: "",
+	information: "",
+},
+{
+	title: "What was Europe like before and after?",
+	stat: "",
+	information: "",
+},
+{
+	title: "Were there any more outbreaks?",
+	stat: "",
+	information: "",
+},
+{
+	title: "Where can I find out more?",
 	stat: "",
 	information: "",
 });

b8e6dcca3df1a657962f5e010704d7b84d807e7f
Victor Cintron <hotslug@gmail.com>
2018-04-30 16:23:53 +0100

    Adds css and info.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 const Header = () => (
-  <header className="wrapper">
+  <header className="header-wrapper">
     <h1 className="replace">The Black Death</h1>
     <Navbar />
   </header>

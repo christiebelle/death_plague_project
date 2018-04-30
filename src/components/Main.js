4a01bcba817b172cc7f995d56dcf1ac8e10ba4b8
Christiebelle <cslester@outlook.com>
2018-04-30 17:57:09 +0100

    ajax request created in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,10 +9,23 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      factsArray: []
     }
   }
 
+componentDidMount(){
+  const request = new XMLHttpRequest();
+    request.open('GET', "http://localhost:3001/facts");
+    request.onload = () => {
+      if (request.status === 200) {
+        const jsonString = request.responseText;
+        const factsArray = JSON.parse(jsonString)
+        this.setState({songs: factsArray});
+      }
+    };
+    request.send();
+    console.log(factsArray);
+}
 
   render() {
     return(

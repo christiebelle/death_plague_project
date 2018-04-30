9545aa816bbdacece1bac1984a692f807b0ae305
Steven Ward <34653526+stvnwrd@users.noreply.github.com>
2018-04-30 14:17:58 +0100

    added new structure to home

---
+++ src/components/Info.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Info from "../components/Info";
+
+export default Info;
---
+++ src/components/Map.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Map from "../components/Map";
+
+export default Map;

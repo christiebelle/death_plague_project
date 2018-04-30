42ef12b9ba9f1b1d3687213e44684a2c2175a4fc
Steven Ward <34653526+stvnwrd@users.noreply.github.com>
2018-04-30 11:51:13 +0100

    deal with splash

---
+++ src/components/Splash.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Splash = () => (
+  <div>
+    <h1>Splash Death</h1>
+    <h2>Welcome Traveller! </h2>
+  <h4>Enter, if thou darest...</h4>
+  </div>
+);
+
+
+export default Splash;

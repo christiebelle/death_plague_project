21264a41d64d3c7d9941e0a3cfa8d55845c00a05
Christiebelle <cslester@outlook.com>
2018-04-30 19:02:23 +0100

    changed to a fetch request and seems to be working

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,22 +9,26 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      factsArray: []
+      facts: []
     }
-    console.log(this.state.factsArray);
   }
 
 componentDidMount(){
-  const request = new XMLHttpRequest();
-    request.open('GET', "http://localhost:3001/facts");
-    request.onload = () => {
-      if (request.status === 200) {
-        const jsonString = request.responseText;
-        const factsArray = JSON.parse(jsonString)
-        this.setState({songs: factsArray});
-      }
-    };
-    request.send();
+fetch("http://localhost:3001/facts")
+.then(response => response.json())
+.then(json => this.setState({facts: json}))
+  //
+  // const request = new XMLHttpRequest();
+  //   request.open('GET', "https://localhost:3001/facts");
+  //   request.onload = () => {
+  //     if (request.status === 200) {
+  //       const jsonString = request.responseText;
+  //       const factsArray = JSON.parse(jsonString);
+  //       this.setState({facts: factsArray});
+  //     }
+  //   };
+  //   request.send();
+  //   console.log(this.state.facts);
 }
 
   render() {

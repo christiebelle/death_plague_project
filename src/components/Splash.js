7941733a389a794f6f385c988d13e73cbbf3b970
Victor Cintron <hotslug@gmail.com>
2018-04-30 14:59:56 +0100

    Adds button to splash page.

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,9 +1,12 @@
 import React from "react";
+import EnterButton from "./EnterButton";
+
 
 const Splash = () => (
 <React.Fragment>
 <div id="wrapper">
 <section id="main-image">
+<EnterButton />
 </section>
 <canvas id='splash-canvas'>
 </canvas>

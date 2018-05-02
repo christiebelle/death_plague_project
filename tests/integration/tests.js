ccb221da8ae02a3fbd2b0263064bf354418498af
Christiebelle <cslester@outlook.com>
2018-05-02 14:20:00 +0100

    selenium running and test failing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -8,12 +8,13 @@ describe('website functionality', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://localhost:3000');
   });
+});
 
   // write integration tests here in the form of "it should do something..."
   it('should have a landing page', function(){
-    browser.get('/');
-    document.querySelector('.enter').click();
-    browser.sleep(1000).then(function() {
+    browser.get('http://localhost:3000');
+    element(by.css("#enter")).click();
+    browser.sleep(1000).then(function(){
       expect(browser.getCurrentUrl()).toBe(homeUrl);
-    });
+    })
   });

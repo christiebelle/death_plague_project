efd48291b94750f4ad98d0e3f9735c0ca2b39d9f
Christiebelle <cslester@outlook.com>
2018-05-02 14:26:14 +0100

    test brings up a separate testing window

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -11,10 +11,12 @@ describe('website functionality', function() {
 });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have a landing page', function(){
+  it('should have a landing page', function(done){
+    this.timeout(15000);
     browser.get('http://localhost:3000');
     element(by.css("#enter")).click();
-    browser.sleep(1000).then(function(){
+    browser.sleep(1000).then(function(done){
       expect(browser.getCurrentUrl()).toBe(homeUrl);
     })
+    done();
   });
